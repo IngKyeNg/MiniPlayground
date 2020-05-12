4fffc7c4d4b5b6044e6bb1b328220105ed61ed91
IngKyeNg <ingkye.ng@c02wm9xahtd5-c.lan>
2020-05-12 10:07:48 +0800

    Added special character checking

--- InterpretSentence/src/com/nik/InterpretSentence.java
+++ InterpretSentence/src/com/nik/InterpretSentence.java
@@ -3,14 +3,9 @@ package com.nik;
 import java.util.Scanner;
 
 public class InterpretSentence {
-    public static void main(String[] args) {
 
-        System.out.println("Enter some sentences and I will tell you how many vowels and consonants in there.");
-
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-
-        char[] alphabets = input.toLowerCase().toCharArray();
+    public static void checkForVowelsAndConsonents(String sentence){
+        char[] alphabets = sentence.toLowerCase().toCharArray();
         int v = 0;
         int c = 0;
 
@@ -18,13 +13,26 @@ public class InterpretSentence {
             if(alphabets[i] == 'a' || alphabets[i] == 'e' || alphabets[i] == 'i' ||
                     alphabets[i] == 'o' || alphabets[i] == 'u' || alphabets[i] == 'y'){
                 v += 1;
-            } else if(alphabets[i] != ' '){
+            }else if(alphabets[i] >= 'a' && alphabets[i] <= 'z'){
                 c += 1;
+            }else{
+                System.out.println("This charcter [" + alphabets[i] + "] is a special characters");
             }
         }
+
         System.out.println("The sentence above have \n" +
                 "Vowels : " + v + "\n" +
                 "Consonants : " + c);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter some sentences and I will tell you how many vowels and consonants in there.");
+
+        Scanner scanner = new Scanner(System.in);
+        String sentence = scanner.nextLine();
+
+        checkForVowelsAndConsonents(sentence);
 
     }
 }
